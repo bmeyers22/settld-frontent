a98f3306152e8ccb971fec95f497733516205ac9
Brian Meyers <meyers.briant@gmail.com>
2015-04-02 00:39:08 +0000

    Changed brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -4,7 +4,8 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
   fingerprint: {
-    prepend: 'https://s3.amazonaws.com/'
+    enabled: true, 
+    prepend: 'https://s3.amazonaws.com/app.settld.com/'
   }
 });
 
