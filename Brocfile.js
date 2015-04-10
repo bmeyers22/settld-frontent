54088cc35132cb5089ee5961796d2d9614de304b
Brian Meyers <meyers.briant@gmail.com>
2015-04-10 06:10:09 -0400

    Fixed feed css

--- Brocfile.js
+++ Brocfile.js
@@ -7,7 +7,7 @@ var isProductionLikeBuild = ['production', 'staging'].indexOf(env) > -1;
 
 var app = new EmberApp({
   fingerprint: {
-    enabled: isProductionLikeBuild, 
+    enabled: isProductionLikeBuild,
     prepend: 'https://s3.amazonaws.com/app.settld.com/'
   }
 });
