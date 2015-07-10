3f4ac1abebd0e5ea4da915fda43a13753f38f4a3
Brian Meyers <meyers.briant@gmail.com>
2015-07-10 17:03:10 -0400

    Completed upgrade

--- app/helpers/fuzzy-time.js
+++ app/helpers/fuzzy-time.js
@@ -1,5 +1,4 @@
-import Ember from 'ember'
+import Ember from 'ember';
+let moment = window.moment;
 
-export default Ember.Helper.helper(function(params, hash) {
-  return moment(params[0]).fromNow()
-});
+export default Ember.Helper.helper( (params) => { return moment(params[0]).fromNow(); } );
---
+++ ember-cli-build.js
@@ -0,0 +1,36 @@
+/* global require, module */
+var EmberApp = require('ember-cli/lib/broccoli/ember-app'),
+  env = EmberApp.env();
+
+var isProductionLikeBuild = ['production', 'staging'].indexOf(env) > -1;
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    fingerprint: {
+      enabled: isProductionLikeBuild,
+      prepend: 'https://s3.amazonaws.com/app.settld.com/'
+    }
+  });
+
+  // Use `app.import` to add additional libraries to the generated
+  // output files.
+  app.import('vendor/javascripts/jquery-ui.js');
+  app.import('bower_components/lodash/lodash.min.js');
+  app.import('vendor/javascripts/socket.io.js');
+  app.import('bower_components/moment/min/moment.min.js');
+  app.import('bower_components/pikaday/css/pikaday.css');
+  app.import('bower_components/pikaday/pikaday.js');
+  app.import('bower_components/ember-pikaday/build/lib.js');
+
+  // If you need to use different assets in different
+  // environments, specify an object as the first parameter. That
+  // object's keys should be the environment name and the values
+  // should be the asset to use in that environment.
+  //
+  // If the library that you are including contains AMD or ES6
+  // modules that you would like to import into your application
+  // please specify an object with the list of modules as keys
+  // along with the exports of each module as its value.
+
+  return app.toTree();
+};
