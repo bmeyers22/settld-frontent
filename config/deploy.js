cae90a57a7ef49761311c23ee74aa7f436a8cb2e
Brian Meyers <meyers.briant@gmail.com>
2015-09-07 20:51:59 -0400

    Fixed deploy config

--- config/deploy.js
+++ config/deploy.js
@@ -1,43 +1,39 @@
-module.exports = {
-  development: {
-    buildEnv: 'development', // Override the environment passed to the ember asset build. Defaults to 'production'
-    store: {
-      type: 'redis', // the default store is 'redis'
+module.exports = function(environment) {
+  var ENV = {};
+
+  if (environment === 'development') {
+    ENV.build = {
+      environment: 'development'
+    };
+    ENV.store = {
       host: 'localhost',
       port: 6379
-    },
-    assets: {
-      type: 's3', // default asset-adapter is 's3'
-      gzip: false, // if undefined or set to true, files are gziped
-      gzipExtensions: ['js', 'css', 'svg'], // if undefined, js, css & svg files are gziped
+    };
+
+    ENV.assets = {
       accessKeyId: process.env['AWS_ACCESS_KEY'],
       secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY'],
       bucket: 'app.dev.settld.com'
-    }
-  },
+    };
+  }
 
-  staging: {
-    buildEnv: 'staging', // Override the environment passed to the ember asset build. Defaults to 'production'
-    store: {
+  if (environment === 'staging' || environment === 'production') {
+    ENV.build = {
+      environment: 'production'
+    };
+
+    ENV.store = {
       host: 'localhost',
       port: 6379
-    },
-    assets: {
-      accessKeyId: process.env['AWS_ACCESS_KEY'],
-      secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY'],
-      bucket: 'app.settld.com'
-    }
-  },
+    };
 
-   production: {
-    store: {
-      host: 'localhost',
-      port: 6379,
-    },
-    assets: {
+    ENV.assets = {
       accessKeyId: process.env['AWS_ACCESS_KEY'],
       secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY'],
-      bucket: 'app.settld.com'
-    }
+      bucket: 'app.settld.com',
+      prefix: '<optional-remote-prefix>'
+    };
   }
-};
+
+  return ENV;
+}
