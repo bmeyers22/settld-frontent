cff38bcd231e9e0c12680ada2de2564c90ec8ef4
Brian Meyers <brian.meyers@spongecell.com>
2015-09-16 13:56:39 -0400

    Update app.js

--- app/routes/app.js
+++ app/routes/app.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return {
-      invoices: new Ember.Set.create()
+      invoices: Ember.Set.create()
     }
   },
   actionBarMap: {
