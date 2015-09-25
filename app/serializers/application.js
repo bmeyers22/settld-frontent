15ba7d9d2166a8ed3fdf07245c03ef7e369221b2
Brian Meyers <meyers.briant@gmail.com>
2015-09-25 11:53:00 -0400

    Fixed serializer and some css

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,7 +1,18 @@
 import { ActiveModelSerializer } from 'active-model-adapter';
 
 var ApplicationSerializer = ActiveModelSerializer.extend({
-  primaryKey: '_id'
+  primaryKey: '_id',
+  serializeHasMany(record, json, relationship) {
+    var key = relationship.key;
+    // don't care which kind of hasMany relationship this is
+    let records = Ember.get(record, key);
+    if (records) {
+      return json[key] = records.mapBy('id');
+    } else {
+      return json[key] = []
+    }
+
+  }
 });
 
 
