29059bb37f50aad833a494ce7058dd1cad952510
Brian Meyers <meyers.briant@gmail.com>
2015-10-02 10:25:36 -0400

    Got rid of corova

--- config/environment.js
+++ config/environment.js
@@ -43,19 +43,19 @@ module.exports = function(environment) {
       }
     },
     contentSecurityPolicy: {
-      'default-src': "'self'",
-      'script-src': "'self' 'unsafe-inline' https://d37gvrvc0wt4s1.cloudfront.net sandbox-api.venmo.com https://www.google-analytics.com/analytics.js http://www.google-analytics.com/analytics.js connect.facebook.net localhost",
-      'font-src': "'self' fonts.gstatic.com data: fonts.googleapis.com", // Allow fonts to be loaded from http://fonts.gstatic.com
-      'connect-src': "'self' sandbox-api.venmo.com https://api.rollbar.com ws://localhost:7000 https://www.google-analytics.com http://www.google-analytics.com",
-      'img-src': "'self' data: venmopics.appspot.com https://www.google-analytics.com http://www.google-analytics.com https://fbcdn-profile-a.akamaihd.net",
-      'style-src': "'self' 'unsafe-inline' fonts.googleapis.com ", // Allow inline styles and loaded CSS from http://fonts.googleapis.com
-      'media-src': "'self'"
+      'default-src': ''self'',
+      'script-src': ''self' 'unsafe-inline' https://d37gvrvc0wt4s1.cloudfront.net sandbox-api.venmo.com https://www.google-analytics.com/analytics.js http://www.google-analytics.com/analytics.js connect.facebook.net localhost',
+      'font-src': ''self' fonts.gstatic.com data: fonts.googleapis.com', // Allow fonts to be loaded from http://fonts.gstatic.com
+      'connect-src': ''self' sandbox-api.venmo.com https://api.rollbar.com ws://localhost:7000 https://www.google-analytics.com http://www.google-analytics.com',
+      'img-src': ''self' data: venmopics.appspot.com https://www.google-analytics.com http://www.google-analytics.com https://fbcdn-profile-a.akamaihd.net',
+      'style-src': ''self' 'unsafe-inline' fonts.googleapis.com ', // Allow inline styles and loaded CSS from http://fonts.googleapis.com
+      'media-src': ''self''
     }
   };
 
   if (environment === 'development') {
     ENV.GA = {
-      UA_CODE: "UA-51059302-1" // where UA code looks something like: UA-00000000-1
+      UA_CODE: 'UA-51059302-1' // where UA code looks something like: UA-00000000-1
     }
 
     // ENV.APP.LOG_RESOLVER = true;
@@ -63,18 +63,18 @@ module.exports = function(environment) {
     ENV.APP.LOG_TRANSITIONS = true;
     ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
-    ENV.PROXY_URL = "http://localhost:3000"
-    ENV['simple-auth']['serverTokenEndpoint'] = 'http://localhost:3000/users/sign_in';
-    ENV['simple-auth-devise']['serverTokenEndpoint'] = 'http://localhost:3000/users/sign_in';
+    ENV.PROXY_URL = ''
+    ENV['simple-auth']['serverTokenEndpoint'] = '/users/sign_in';
+    ENV['simple-auth-devise']['serverTokenEndpoint'] = '/users/sign_in';
     ENV.torii.providers = {
       'facebook-oauth2': {
         apiKey: process.env['FACEBOOK_APP_ID_SETTLD_TEST'],
         scope: 'user_birthday, user_location, user_about_me, email, public_profile',
-        redirectUri: "http://localhost:4200/login"
+        redirectUri: 'http://localhost:4200/login'
       },
       'venmo-oauth2': {
         apiKey: process.env['VENMO_APP_ID_SETTLD_TEST'],
-        redirectUri: "http://localhost:4200/register",
+        redirectUri: 'http://localhost:4200/register',
         scope: 'access_email, access_phone, access_profile, make_payments'
       }
     }
@@ -83,7 +83,7 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.PROXY_URL = "http://app.settld.com/";
+    ENV.PROXY_URL = '';
     ENV.baseURL = '/';
 
     // keep test console output quieter
@@ -95,21 +95,21 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.GA = {
-      UA_CODE: "UA-51059302-1" // where UA code looks something like: UA-00000000-1
+      UA_CODE: 'UA-51059302-1' // where UA code looks something like: UA-00000000-1
     }
-    ENV.PROXY_URL = "http://app.settld.com";
-    ENV['simple-auth']['serverTokenEndpoint'] = 'http://app.settld.com/users/sign_in';
-    ENV['simple-auth-devise']['serverTokenEndpoint'] = 'http://app.settld.com/users/sign_in';
+    ENV.PROXY_URL = '';
+    ENV['simple-auth']['serverTokenEndpoint'] = ENV.PROXY_URL + '/users/sign_in';
+    ENV['simple-auth-devise']['serverTokenEndpoint'] = ENV.PROXY_URL + '/users/sign_in';
     ENV.torii.providers = {
       'facebook-oauth2': {
         apiKey: process.env['FACEBOOK_APP_ID_SETTLD'],
         scope: 'user_birthday, user_location, user_about_me, email, public_profile',
-        redirectUri: "http://app.settld.com/login",
+        redirectUri: ENV.PROXY_URL + '/login',
       },
       'venmo-oauth2': {
         apiKey: process.env['VENMO_APP_ID_SETTLD'],
         scope: 'access_email, access_phone, access_profile, make_payments',
-        redirectUri: "http://app.settld.com/register",
+        redirectUri: ENV.PROXY_URL + '/register',
       }
     }
     ENV.production = true;
