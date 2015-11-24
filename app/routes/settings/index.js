002effc68a5a28cb01829120aa73b31adc0d4e0c
Brian Meyers <meyers.briant@gmail.com>
2015-11-24 03:03:36 -0500

    removed deprecated libraries

--- app/routes/settings/index.js
+++ app/routes/settings/index.js
@@ -2,23 +2,22 @@ import Ember from 'ember';
 import config from 'web/config/environment';
 
 export default Ember.Route.extend({
-  currentSession: Ember.inject.service(),
-  model() {
-    return this.get('currentSession.authUser');
-  },
-  actions: {
-    connect(provider){
-      let session = this.get('session');
-      this.get('torii').open("venmo-oauth2").then((data) => {
-        return this.get('currentSession').linkVenmo(data);
-      })
+    currentSession: Ember.inject.service(),
+    model() {
+        return this.get('currentSession.authUser');
     },
-    togglePrivacy() {
-      let self = this;
-      self.set('currentModel.settings.hasPublicProfile', !self.get('currentModel.settings.hasPublicProfile'));
-      Ember.run(function () {
-        self.get('currentModel.settings').save();
-      });
+    actions: {
+        connect(provider){
+            let session = this.get('session');
+            this.get('torii').open("venmo-oauth2").then((data) => {
+                return this.get('currentSession').linkVenmo(data);
+            })
+        },
+        togglePrivacy() {
+            Ember.run( () => {
+                this.set('currentModel.settings.hasPublicProfile', !this.get('currentModel.settings.hasPublicProfile'));
+                this.get('currentModel.settings').save();
+            });
+        }
     }
-  }
 });
